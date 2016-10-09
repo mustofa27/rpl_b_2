5be28a5117fe7ca3739bd46d74f333b44fc7dc74
mustofa27 <buffonmustofa@gmail.com>
2016-10-09 16:58:27 +0700

    asp.net project

---
+++ IRCI/IRCI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IRCI.MvcApplication" Language="C#" %>
---
+++ IRCI/IRCI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IRCI.Startup))]
+namespace IRCI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
