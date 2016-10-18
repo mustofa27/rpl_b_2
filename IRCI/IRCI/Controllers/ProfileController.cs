960a0944be8bd57bcb43826bad362deb3945f542
An'im <animfahmy@yahoo.co.id>
2016-10-18 08:13:30 +0700

    - Add action in search form - Change language to english

---
+++ IRCI/IRCI/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IRCI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profil
+        public ActionResult Index(int id = 0)
+        {
+            //int profilId = id; //the id in the URL
+            //string data = RouteData.Values["id"] + Request.Url.Query;
+            //string data = RouteData.Values["id"].ToString(); ;
+            //ViewData["data"] = data;
+            ViewData["id"] = id;
+            return View();
+        }
+    }
+}
--- IRCI/IRCI/Controllers/SearchController.cs
+++ IRCI/IRCI/Controllers/SearchController.cs
@@ -6,13 +6,15 @@ using System.Web.Mvc;
 
 namespace IRCI.Controllers
 {
-    public class MencariController : Controller
+    public class SearchController : Controller
     {
         //
         // GET: /Mencari/
 
         public ActionResult Index()
         {
+            string keyword = Request.Form["keyword"];
+            ViewBag.keyword = keyword;
             return View();
         }
 
