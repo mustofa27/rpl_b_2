048f3f84c6bb5fa37f64fd6882eb95d44452ef7d
An'im <animfahmy@yahoo.co.id>
2016-10-18 09:56:39 +0700

    - Install Npgsql - Try first to use connection to postgre in search result

--- IRCI/IRCI/Controllers/SearchController.cs
+++ IRCI/IRCI/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,32 @@ namespace IRCI.Controllers
         {
             string keyword = Request.Form["keyword"];
             ViewBag.keyword = keyword;
-            return View();
+
+            List<string> result = new List<string>();
+            using (var conn = new NpgsqlConnection("Host=127.0.0.1;Username=postgres;Password=root;Database=rpl"))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    // Insert some data
+                    //cmd.CommandText = "INSERT INTO data (some_field) VALUES ('Hello world')";
+                    //cmd.ExecuteNonQuery();
+
+                    // Retrieve all rows
+                    cmd.CommandText = "SELECT * FROM (SELECT UNNEST(author_creator) author_creator, LOWER(UNNEST(author_creator)) author_tag FROM irci.records) t1 WHERE author_tag LIKE '%"+ keyword + "%' LIMIT 10";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            //Console.WriteLine(reader.GetString(0));
+                            result.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+            return View(result);
         }
 
         public ActionResult adminlte()
