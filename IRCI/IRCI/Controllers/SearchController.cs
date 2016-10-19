22e1781eba7e817ed260ca9efca8047b32c4c369
An'im <animfahmy@yahoo.co.id>
2016-10-19 09:10:31 +0700

    Search results from database and get random photo from internet

--- IRCI/IRCI/Controllers/SearchController.cs
+++ IRCI/IRCI/Controllers/SearchController.cs
@@ -1,47 +1,22 @@
-﻿using Npgsql;
-using System;
+﻿using System.Web.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using IRCI.Models;
 
 namespace IRCI.Controllers
 {
     public class SearchController : Controller
     {
+        private List<RecordsAuthor> records;
+        private RecordsAuthorClass RecordsAuthor = new RecordsAuthorClass();
         //
         // GET: /Mencari/
 
-        public ActionResult Index()
+        public ActionResult Index(int page = 0)
         {
             string keyword = Request.Form["keyword"];
             ViewBag.keyword = keyword;
-
-            List<string> result = new List<string>();
-            using (var conn = new NpgsqlConnection("Host=127.0.0.1;Username=postgres;Password=root;Database=rpl"))
-            {
-                conn.Open();
-                using (var cmd = new NpgsqlCommand())
-                {
-                    cmd.Connection = conn;
-
-                    // Insert some data
-                    //cmd.CommandText = "INSERT INTO data (some_field) VALUES ('Hello world')";
-                    //cmd.ExecuteNonQuery();
-
-                    // Retrieve all rows
-                    cmd.CommandText = "SELECT * FROM (SELECT UNNEST(author_creator) author_creator, LOWER(UNNEST(author_creator)) author_tag FROM irci.records) t1 WHERE author_tag LIKE '%"+ keyword + "%' LIMIT 10";
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            //Console.WriteLine(reader.GetString(0));
-                            result.Add(reader.GetString(0));
-                        }
-                    }
-                }
-            }
-            return View(result);
+            records = RecordsAuthor.getRecords(page * 10, 10, keyword);
+            return View(records);
         }
 
         public ActionResult adminlte()
