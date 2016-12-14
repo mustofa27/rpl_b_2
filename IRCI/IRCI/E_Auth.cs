2a5df2cf41f1937d8fc14de1ed12c3565ab6a108
Ryan <ryan.adhisa@gmail.com>
2016-12-14 09:55:47 +0700

    a

---
+++ IRCI/IRCI/E_Auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IRCI.Entity
+{
+    public class E_Auth
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ IRCI/IRCI/E_Records.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IRCI.Entity
+{
+    public class E_Records
+    {
+        public string id_record { get; set; }
+
+    }
+}
